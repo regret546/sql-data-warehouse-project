76e7f793abe5a54cba262de8a7909a6a3aa8d24f
regret546 <iregret546@gmail.com>
2026-01-24 01:21:28 +0800

    finalize gold layer

--- scripts/gold/views.sql
+++ scripts/gold/views.sql
@@ -18,3 +18,44 @@ LEFT JOIN silver.erp_cust_az12 ca
 ON ci.cst_key = ca.cid
 LEFT JOIN silver.erp_loc_a101 la
 ON ci.cst_key = la.cid
+
+---------------------------------------------------------------------------------------------------------------------
+
+CREATE VIEW gold.dim_products AS 
+SELECT 
+	ROW_NUMBER() OVER (ORDER BY pn.prd_start_date, pn.prd_key) AS product_key,
+	pn.prd_id AS product_id,
+	pn.prd_key AS product_number,
+	pn.prd_nm AS product_name,
+	pn.cat_id AS category_id,
+	pc.cat AS category,
+	pc.subcat AS subcategory,
+	pc.main AS maintenance,
+	pn.prd_cost AS cost,
+	pn.prd_line AS product_line,
+	pn.prd_start_date AS start_date
+FROM silver.crm_prd_info pn
+LEFT JOIN silver.erp_px_cat_g1v2 pc
+ON pn.cat_id = pc.id
+WHERE pn.prd_end_date IS NULL -- Filter out all historical data
+
+---------------------------------------------------------------------------------------------------------------------
+
+CREATE VIEW gold.fact_sales AS
+SELECT 
+	sd.sls_ord_num AS order_num,
+	pr.product_key ,
+	cu.customer_key,
+	sd.sls_order_date AS order_date,
+	sd.sls_ship_date AS shipping_date,
+	sd.sls_due_date AS due_date,
+	sd.sls_sales AS sales_amount,
+	sd.sls_quantity AS quantity,
+	sd.sls_price AS price
+FROM silver.crm_sales_details sd
+LEFT JOIN gold.dim_products pr
+ON sd.sls_prd_key = pr.product_number
+LEFT JOIN gold.dim_customers cu
+ON sd.sls_cust_id = cu.customer_id
+
+
